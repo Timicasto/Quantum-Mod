800a27302d9806ae62c653198ef8a250a5b051a6
Timicasto Index <timicasto@gmail.com>
2020-10-04 23:56:00 +0800

    First commit

--- src/main/java/timicasto/quantumbase/QuantumBase.java
+++ src/main/java/timicasto/quantumbase/QuantumBase.java
@@ -2,9 +2,12 @@ package timicasto.quantumbase;
 
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
+import timicasto.quantumbase.proxy.CommonProxy;
 
 @Mod(modid = QuantumBase.MODID, name = QuantumBase.NAME, version = QuantumBase.VERSION)
 public class QuantumBase {
@@ -14,15 +17,26 @@ public class QuantumBase {
 
     private static Logger logger;
 
+    @SidedProxy(clientSide = "timicasto.quantumbase.proxy.ClientProxy",serverSide = "timicasto.quantumbase.proxy.CommonProxy")
+    public static CommonProxy proxy;
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+        proxy.preInit(event);
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
         logger.info("Initializing Quantum Base");
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postInit(event);
     }
 }
