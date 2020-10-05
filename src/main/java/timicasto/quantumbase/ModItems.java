e7fe949bfa981c611d407c962ed0b2c53b9c0f97
Lama3L9R <Guzhipei@outlook.com>
2020-10-05 22:15:28 +0800

    更新注册方案以及主类变量

--- src/main/java/timicasto/quantumbase/ModItems.java
+++ src/main/java/timicasto/quantumbase/ModItems.java
@@ -6,6 +6,23 @@ import net.minecraft.item.ItemBlock;
 import timicasto.quantumbase.block.WillowWood;
 
 public class ModItems {
-    public static Block willowWood = new WillowWood();
-    public static Item willowWoodItemBlock = new ItemBlock(willowWood);
+    private static HashMap<String, Item> itemRegisterMap = new HashMap<>();
+    private static HashMap<String, Block> blockRegisterMap = new HashMap<>();
+
+    {
+        blockRegisterMap.put("b:willow_wood", new WillowWood());
+        itemRegisterMap.put("willow_wood", new ItemBlock(willowWood));
+    }
+
+    public static void registerItems(Registry<Item> registry) {
+        itemRegisterMap.forEach((k, v) -> {
+            registry.register(v.setRegistryName(k));
+        });
+    }
+
+    public static void registerBlocks(Registry<Block> registry) {
+        blockRegisterMap.values().forEach((v) -> {
+            registry.register(v);
+        });
+    }
 }
