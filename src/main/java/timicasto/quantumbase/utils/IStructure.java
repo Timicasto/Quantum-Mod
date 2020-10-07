8fbb3804a817ebe767c7f814d1472dba724edb6b
Timicasto Index <timicasto@gmail.com>
2020-10-07 19:53:10 +0800

    updated Combustible Ice Generate

--- src/main/java/timicasto/quantumbase/utils/IStructure.java
+++ src/main/java/timicasto/quantumbase/utils/IStructure.java
@@ -7,7 +7,7 @@ import net.minecraft.world.gen.structure.template.PlacementSettings;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public interface IStructure {
-    public static final WorldServer worldServer = FMLCommonHandler.instance().getMinecraftServerInstance().getWorld(0);
-    public static final PlacementSettings settings = new PlacementSettings().setChunk(null).setIgnoreEntities(false).setIgnoreStructureBlock(false).setMirror(Mirror.NONE).setRotation(Rotation.NONE);
+    WorldServer worldServer = FMLCommonHandler.instance().getMinecraftServerInstance().getWorld(0);
+    PlacementSettings settings = new PlacementSettings().setChunk(null).setIgnoreEntities(false).setIgnoreStructureBlock(false).setMirror(Mirror.NONE).setRotation(Rotation.NONE);
 
 }
