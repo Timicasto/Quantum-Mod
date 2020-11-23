b20c487e4dadf1e108cab267a320895588f3b145
Eyre_S <Sukazyo@outlook.com>
2020-11-23 22:19:05 +0800

    添加赤铁矿和磁铁矿的模型材质，金属熔炼炉取消蹲下打开

--- src/main/java/timicasto/quantumbase/block/BlockMetalSmelter.java
+++ src/main/java/timicasto/quantumbase/block/BlockMetalSmelter.java
@@ -16,8 +16,6 @@ import org.apache.logging.log4j.Logger;
 import timicasto.quantumbase.GuiHandler;
 import timicasto.quantumbase.QuantumBase;
 import timicasto.quantumbase.creative.TabLoader;
-import timicasto.quantumbase.fluid.FluidLoader;
-import timicasto.quantumbase.tile.TileEntityKeroseneLight;
 import timicasto.quantumbase.tile.TileEntityMetalSmelter;
 
 public class BlockMetalSmelter extends Block {
@@ -41,15 +39,19 @@ public class BlockMetalSmelter extends Block {
 
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-            int id = GuiHandler.guiMetalSmelter;
-            if (playerIn.isSneaking()) {
-                playerIn.openGui(QuantumBase.MODID, id, worldIn, pos.getX(), pos.getY(), pos.getZ());
-                logger.info("OPENED GUI : " + id);
-            }
+        
         TileEntity tile = worldIn.getTileEntity(pos);
-        if (tile instanceof TileEntityMetalSmelter && playerIn.getHeldItem(hand).getItem() == Items.WATER_BUCKET) {
-            playerIn.setHeldItem(hand, ((TileEntityMetalSmelter)tile).tryAcceptFuel());
-            playerIn.sendStatusMessage(new TextComponentString("Fuel: " + ((TileEntityMetalSmelter)tile).getCO()), true);
+        if (tile instanceof TileEntityMetalSmelter) {
+            if (playerIn.getHeldItem(hand).getItem() == Items.WATER_BUCKET) {
+                playerIn.setHeldItem(hand, ((TileEntityMetalSmelter)tile).tryAcceptFuel());
+                playerIn.sendStatusMessage(new TextComponentString("Fuel: " + ((TileEntityMetalSmelter)tile).getCO()), true);
+            } else {
+                int id = GuiHandler.guiMetalSmelter;
+                if (playerIn.isSneaking()) {
+                    playerIn.openGui(QuantumBase.MODID, id, worldIn, pos.getX(), pos.getY(), pos.getZ());
+                    logger.info("OPENED GUI : " + id);
+                }
+            }
         }
         return true;
     }
