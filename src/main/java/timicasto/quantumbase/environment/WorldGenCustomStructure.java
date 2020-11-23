e9f4ea9707c657668cd9fd04c04f3e5402ce2656
Eyre_S <Sukazyo@outlook.com>
2020-11-23 19:38:04 +0800

    添加一些javadoc和注释

--- src/main/java/timicasto/quantumbase/environment/WorldGenCustomStructure.java
+++ src/main/java/timicasto/quantumbase/environment/WorldGenCustomStructure.java
@@ -18,38 +18,53 @@ public class WorldGenCustomStructure implements IWorldGenerator {
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
         switch (world.provider.getDimension()) {
-            case 1:
-                break;
             case 0:
+                // combustible ice generation
                 if (world.getBiomeForCoordsBody(new BlockPos(chunkX * 16, 70, chunkZ * 16)) instanceof BiomeOcean) {
                     generateStructure(combustibleIce, world, random, chunkX, chunkZ, 1, Blocks.WATER);
                 }
                 break;
-            case -1:
-                break;
         }
     }
+    
+    /**
+     * 在距顶层方块的5层之下生成指定结构
+     *
+     * @param generator 指定结构的生成器
+     * @param world 所处世界
+     * @param random dunno
+     * @param chunkX 区块的X轴坐标
+     * @param chunkZ 区块的Z轴坐标
+     * @param chance 生成机会
+     * @param topBlock 顶层方块
+     */
     private void generateStructure(WorldGenerator generator, World world, Random random, int chunkX, int chunkZ, int chance, Block topBlock) {
-
-
         int x = (chunkX * 16) + random.nextInt(15);
         int z = (chunkZ * 16) + random.nextInt(15);
-        int y = calculateGeneartionHeight(world,x,z,topBlock);
+        int y = calculateGenerationHeight(world,x,z,topBlock);
         BlockPos pos = new BlockPos(x,y,z);
           if (random.nextInt(chance) == 0) {
                 generator.generate(world,random,pos);
             }
     }
-
-    private static int calculateGeneartionHeight(World world, int x, int z, Block topBlock) {
+    
+    /**
+     * 计算出以指定的顶层方块向下五格的方块位置
+     *
+     * @param world 所处世界
+     * @param x X轴坐标
+     * @param z Z轴坐标
+     * @param topBlock 指定的顶层方块
+     * @return 比顶层方块低5格的高度
+     */
+    private static int calculateGenerationHeight(World world, int x, int z, Block topBlock) {
         int y = world.getHeight();
         boolean foundGround = false;
         while(!foundGround && y-- >= 0) {
             Block block = world.getBlockState(new BlockPos(x,y,z)).getBlock();
             foundGround = block == topBlock;
         }
-        int resultY = y-5;
-        return resultY;
+        return y-5;
     }
 }
 
--- src/main/java/timicasto/quantumbase/item/ItemDebugTool.java
+++ src/main/java/timicasto/quantumbase/item/ItemDebugTool.java
@@ -21,30 +21,36 @@ public class ItemDebugTool extends Item {
 
     @Override
     public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        player.sendMessage(new TextComponentString(String.format("" +
-                                                                         "§l§m================================================================§r\n" +
-                                                                         "   §0$0 §1$1 §2$2 §3$3 §4$4 §5$5 §6$6 §7$7 §8$8 §9$9 §a$a §b$b §c$c §d$d §e$e §f$f   §r§k$k§r  §l$l§r  §m$m§r  §n$n§r  §o$o§r  §r$r\n" +
-                                                                         "§l§m================================================================§r\n" +
-                                                                         "                               §a§lExtra Message\n" +
-                                                                         "\n"  +
-                                                                         "\n"  +
-                                                                         "\n"  +
-                                                                         "\n"  +
-                                                                         "\n"  +
-                                                                         "\n"  +
-                                                                         "\n"  +
-                                                                         " §7§otodo\n"  +
-                                                                         "§l----------------------------------------------------------------§r\n" +
-                                                                         "                        §a§lQuantum Dev Message Summoned\n" +
-                                                                         "\n"  +
-                                                                         "  §lCurrent Player: §r§e%s §r§7§o%s\n"+
-                                                                         "  §lFacing Block: §r§e%s §r§7§o%s\n"  +
-                                                                         "\n" +
-                                                                         "\n" +
-                                                                         "\n" +
-                                                                         " §7§otodo\n" +
-                                                                         "§l§m================================================================§r"
-        , player.getName(), player.getUniqueID().toString(), worldIn.getBlockState(pos).getBlock().getLocalizedName(), worldIn.getBlockState(pos).getBlock().getRegistryName())));
+        if (worldIn.isRemote) {
+            player.sendMessage(new TextComponentString(String.format("" +
+                                                                             "§l§m================================================================§r\n" +
+                                                                             "   §0$0 §1$1 §2$2 §3$3 §4$4 §5$5 §6$6 §7$7 §8$8 §9$9 §a$a §b$b §c$c §d$d §e$e §f$f   §r§k$k§r  §l$l§r  §m$m§r  §n$n§r  §o$o§r  §r$r\n" +
+                                                                             "§l§m================================================================§r\n" +
+                                                                             "                               §a§lExtra Message\n" +
+                                                                             "\n"  +
+                                                                             "\n"  +
+                                                                             "\n"  +
+                                                                             "\n"  +
+                                                                             "\n"  +
+                                                                             "\n"  +
+                                                                             "\n"  +
+                                                                             " §7§otodo\n"  +
+                                                                             "§l----------------------------------------------------------------§r\n" +
+                                                                             "                        §a§lQuantum Dev Message Summoned\n" +
+                                                                             "\n"  +
+                                                                             "  §lCurrent Player: §r§e%s §r§7§o%s\n"+
+                                                                             "  §lFacing Block: §r§e%s §r§7§o%s\n"  +
+                                                                             "\n" +
+                                                                             "\n" +
+                                                                             "\n" +
+                                                                             " §7§otodo\n" +
+                                                                             "§l§m================================================================§r"
+                    , player.getName(),
+                    player.getUniqueID().toString(),
+                    worldIn.getBlockState(pos).getBlock().getLocalizedName(),
+                    worldIn.getBlockState(pos).getBlock().getRegistryName()
+            )));
+        }
         return null;
     }
 }
