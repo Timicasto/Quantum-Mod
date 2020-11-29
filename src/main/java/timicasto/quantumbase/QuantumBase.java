41e4ceae4a6c2c4993ffe37c298f1a519a418294
Eyre_S <Sukazyo@outlook.com>
2020-11-29 20:25:57 +0800

    版本号更改 & 使用 gradle 管理版本号

--- src/main/java/timicasto/quantumbase/QuantumBase.java
+++ src/main/java/timicasto/quantumbase/QuantumBase.java
@@ -26,7 +26,7 @@ import timicasto.quantumbase.proxy.CommonProxy;
 public class QuantumBase {
     public static final String MODID = "quantumbase";
     public static final String NAME = "Quantum Base";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "@version@";
     private static Logger logger = LogManager.getLogger();
 
     private SimpleNetworkWrapper networkWrapper;
